451f27d164a11c34f3bf4cd935de6ab636dabdd1
SidelineCory24 <sidelineadventures24@gmail.com>
2020-05-11 08:32:38 -0400

    initial commit

---
+++ twitter/readme.md
@@ -0,0 +1,23 @@
+## Overview
+This script removes the hassle of having to login to twitter daily to tweet the same tweets.
+
+The daily tweets are stored in a dictonary and are tweeted out every few minutes.
+
+The sleep timer on **Line 56** can be changed to whatever time suits you best.
+`time.sleep(480)`
+
+This script also enables the ability to do adhoc tweets as well.
+
+You will need to have a Twitter account to access the Twitter API.
+[Twitter API Documentation](https://developer.twitter.com/en/docs/basics/getting-started)
+
+
+### Build requirements:
+1. Access to the Twitter API (generates keys and token)
+2. tweepy module (pip install tweepy)
+
+*This was written with Python version 3.8.2*
+
+
+
+ 
---
+++ twitter/tweets.py
@@ -0,0 +1,77 @@
+import tweepy
+import time
+import sys
+
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
+
+
+def OAuth():
+    try:
+        oauth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        oauth.set_access_token(access_token, access_secret)
+        return oauth
+    except Exception as e:
+        return None
+
+
+def tweet_api():
+    oauth = OAuth()
+    api = tweepy.API(oauth)
+    return api
+
+
+def tweets():
+    daily = {'tweet1': "Insert tweet here.",
+             'tweet2': "Insert tweet here.",
+             'tweet3': "Insert tweet here.",
+             }
+    return daily
+
+
+def adhoc_tweet():
+    print('\nAd-hoc Tweet')
+    api = tweet_api()
+    tweet = input('What would you like to tweet?: ')
+
+    print(f'Tweeting...\n{tweet}')
+    api.update_status(tweet)
+    print('Tweet complete.')
+
+
+def daily_tweets(*tweets):
+    print('\nDaily Tweets')
+    api = tweet_api()
+    count = 1
+
+    for tweet in tweets:
+        for t in tweet:
+            if count != len(tweet[t]):
+                print(f'Tweeting Tweet #{count}: {t.capitalize()} Tweet')
+                api.update_status(tweet[t])
+                count += 1
+                time.sleep(480)
+            else:
+                print(f'Tweeting Tweet #{count + 1}: {t.capitalize()}')
+                api.update_status(tweet[t])
+
+    print('Tweets are complete.')
+
+
+if __name__ == '__main__':
+
+    while True:
+        choice = input(
+            'Do you want to do an "adhoc" tweet or the "daily" tweets?: ').lower()
+
+        if choice == 'adhoc' or choice == 'ad-hoc':
+            adhoc_tweet()
+            break
+
+        if choice == 'daily':
+            daily = tweets()
+            daily_tweets(daily)
+            break
