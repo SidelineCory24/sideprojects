01eed93e89f26c021bfcc228eeff6e8d17af6a32
SidelineCory24 <sidelineadventures24@gmail.com>
2020-05-11 13:35:19 -0400

    syntax updates for consitency

--- codechallenges/metaphase/app.js
+++ codechallenges/metaphase/app.js
@@ -1,10 +1,9 @@
-var myChart;
+let myChart;
 
 async function createChart() {
     const name = document.getElementById("playerSearch").value;
     const playerData = await getPlayer(name);
     const context = document.getElementById("myChart").getContext("2d");
-
     const playerLabel = playerData.player.firstName + " " + playerData.player.lastName + " Stats: 2019-2020 ( " + playerData.player.team + " )";
 
     // Handles glitch where multiple results would appear
